ded59ace9885ffef73f615ce36a660784d6762e8
tsm.osm <97128470+oussama12as@users.noreply.github.com>
2023-12-27 03:31:54 +0100

    Navbar file updated

--- resources/views/layouts/Navbar.blade.php
+++ resources/views/layouts/Navbar.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('name', 'Ophtalmologie') }}</title>
-    <link href="{{ asset('assets/img/logo.png') }}" rel="icon">
-    <link href="{{ asset('assets/img/logo.png') }}" rel="apple-touch-icon">
+    <link href="assets/img/logo.png" rel="icon">
+    <link href="assets/img/logo.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -19,11 +19,11 @@
     <script src="https://kit.fontawesome.com/04746b7ca9.js" crossorigin="anonymous"></script>
 
     <!-- Vendor CSS Files -->
-    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/aos/aos.css" rel="stylesheet') }}">
-    <link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
     <!-- Template Main CSS File -->
     <link href="assets/css/variables.css" rel="stylesheet">
 
@@ -124,7 +124,7 @@
                             <li>
                                 <div class="container">
                                     <a class="navbar-brand" href="/" style="color: black">
-                                        <img src="{{ asset('assets/img/logo.png') }}" alt="Ophtalmologie"
+                                        <img src="assets/img/logo.png" alt="Ophtalmologie"
                                             width="40" height="40">
                                         Ophtalmologie
                                     </a>
@@ -361,7 +361,7 @@
             <div class="footer-1">
 
                 <a class="navbar-brand" href="/">
-                    <img src="{{ asset('assets/img/logo.png') }}" alt="Ophtalmologie" width="40"
+                    <img src="assets/img/logo.png" alt="Ophtalmologie" width="40"
                         height="40">
                     Ophtalmologie
                 </a>
@@ -429,32 +429,32 @@
         </div>
     </footer>
     <!-- Vendor JS Files -->
-    <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
-    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
     <!-- jquery -->
-    <script src="{{ asset('assets/js/jquery-1.11.3.min.js') }}"></script>
+    <script src="assets/js/jquery-1.11.3.min.js"></script>
     <!-- bootstrap -->
-    <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <!-- count down -->
-    <script src="{{ asset('assets/js/jquery.countdown.js') }}"></script>
+    <script src="assets/js/jquery.countdown.js"></script>
     <!-- isotope -->
-    <script src="{{ asset('assets/js/jquery.isotope-3.0.6.min.js') }}"></script>
+    <script src="assets/js/jquery.isotope-3.0.6.min.js"></script>
     <!-- waypoints -->
-    <script src="{{ asset('assets/js/waypoints.js') }}"></script>
+    <script src="assets/js/waypoints.js"></script>
     <!-- owl carousel -->
-    <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
     <!-- magnific popup -->
-    <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
     <!-- mean menu -->
-    <script src="{{ asset('assets/js/jquery.meanmenu.min.js') }}"></script>
+    <script src="assets/js/jquery.meanmenu.min.js"></script>
     <!-- sticker js -->
-    <script src="{{ asset('assets/js/sticker.js') }}"></script>
+    <script src="assets/js/sticker.js"></script>
     <!-- main js -->
-    <script src="{{ asset('assets/js/main-blog.js') }}"></script>
+    <script src="assets/js/main-blog.js"></script>
     <!-- Template Main JS File -->
-    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="assets/js/main.js"></script>
     {{-- whatsapp  --}}
     <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
     <div class="elfsight-app-87de5326-af64-4477-928a-1c7c8e1bcd58" data-elfsight-app-lazy></div>
